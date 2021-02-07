6e459b801624a236572dc0c8c7abda9c38f94661
maviola5 <maviola5@gmail.com>
2021-02-07 12:26:56 -0500

    feat(request-logger): add requestId/sourceIp to request logs

--- src/request-logger/index.ts
+++ src/request-logger/index.ts
@@ -5,12 +5,18 @@ const logger = createLogger();
 export const requestLogger = () => async (ctx: Context, next: Next) => {
   const start = Date.now();
   try {
-    logger.info(`Request Start - ${ctx.method} ${ctx.url}`);
+    logger.info({
+      message: `Request Start - ${ctx.method} ${ctx.url}`,
+      requestId: ctx.headers.requestId,
+      sourceIp: ctx.headers.sourceIp,
+    });
     await next();
   } finally {
     const ms = Date.now() - start;
     logger.info({
       message: `Request End - ${ctx.method} ${ctx.url}`,
+      requestId: ctx.headers.requestId,
+      sourceIp: ctx.headers.sourceIp,
       duration: ms + 'ms',
     });
   }
