8e6ffa5e2a38d56d28c5a5a38edb642e504bb842
maviola5 <maviola5@gmail.com>
2021-04-05 08:08:14 -0400

    feat: add not found resource error

--- src/resource-errors/bad-request-error.ts
+++ src/resource-errors/bad-request-error.ts
@@ -7,3 +7,13 @@ export class BadRequestError extends Error {
     this.status = status;
   }
 }
+
+export class NotFoundError extends Error {
+  message: string;
+  status: number;
+  constructor(message = 'Resource not found.', status = 404) {
+    super();
+    this.message = message;
+    this.status = status;
+  }
+}
